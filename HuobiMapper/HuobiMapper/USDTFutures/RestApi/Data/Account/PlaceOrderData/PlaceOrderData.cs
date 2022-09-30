56d2f38f5cb1befe3a2040b438ac380fbbf6c50f
SHOKER <oleg.sarychev.03@gmail.com>
2022-09-30 20:49:04 +0500

    Dev.4

--- HuobiMapper/HuobiMapper/USDTFutures/RestApi/Data/Account/PlaceOrderData/PlaceOrderData.cs
+++ HuobiMapper/HuobiMapper/USDTFutures/RestApi/Data/Account/PlaceOrderData/PlaceOrderData.cs
@@ -6,20 +6,79 @@ namespace HuobiMapper.USDTFutures.RestApi.Data.Account.PlaceOrderData
     {
         [JsonConstructor]
         
-           public PlaceOrderData(string contract_code, int reduce_only, long client_order_id, decimal price, long volume,
-                string direction, string offset, int lever_rate, string order_price_type,decimal tp_trigger_price, decimal tp_order_price,
-                string tp_order_price_type, decimal sl_trigger_price, decimal sl_order_price,
-                string sl_order_price_type)
+           public PlaceOrderData(string сontractCode, int reduceOnly, long clientOrderId, decimal price, long volume,
+                string direction, string offset, int leverRate, string orderPriceType,decimal tpTriggerPrice, decimal tpOrderPrice,
+                string tpOrderPriceType, decimal slTriggerPrice, decimal slOrderPrice,
+                string slOrderPriceType)
            {
-               ContractCode = contract_code;
-               ReduceOnly = reduce_only;
-               ClientOrderId = client_order_id;
+               ContractCode = сontractCode;
+               ReduceOnly = reduceOnly;
+               ClientOrderId = clientOrderId;
                Price = price;
                Volume = volume;
                Direction = direction;
                Offset = offset;
-               Lever_rate = lever_rate;
+               LeverRate = leverRate;
+               OrderPriceType = orderPriceType;
+               TpTriggerPrice = tpTriggerPrice;
+               TpOrderPrice = tpOrderPrice;
+               TpOrderPriceType = tpOrderPriceType;
+               SlTriggerPrice = slTriggerPrice;
+               SlOrderPrice = slOrderPrice;
+               SlOrderPriceType = slOrderPriceType;
            }
-        
+           [JsonProperty("contract_code")]
+           public string ContractCode { get; set; }
+
+           [JsonProperty("reduce_only")]
+           public int ReduceOnly { get; set; }
+           
+           [JsonProperty("client_order_id")]
+           public long ClientOrderId { get; set; }
+           
+           [JsonProperty("price")]
+           public decimal Price { get; set; }
+           
+           [JsonProperty("volume")]
+           public long Volume { get; set; }
+           
+           [JsonProperty("direction")]
+           public string Direction { get; set; }
+           
+           [JsonProperty("offset")]
+           public string Offset { get; set; }
+           
+           [JsonProperty("lever_rate")]
+           public int LeverRate { get; set; }
+
+
+           [JsonProperty("order_price_type")]
+           public string OrderPriceType { get; set; }
+
+
+           [JsonProperty("tp_trigger_price")]
+           public decimal TpTriggerPrice { get; set; }
+
+
+           [JsonProperty("tp_order_price")]
+           public decimal TpOrderPrice { get; set; }
+
+
+           [JsonProperty("tp_order_price_type")]
+           public string TpOrderPriceType { get; set; }
+
+
+           [JsonProperty("sl_trigger_price")]
+           public decimal SlTriggerPrice { get; set; }
+
+
+           [JsonProperty("sl_order_price")]
+           public decimal SlOrderPrice { get; set; }
+
+
+           [JsonProperty("sl_order_price_type")]
+           public string SlOrderPriceType { get; set; }
+
+
     }
 }
